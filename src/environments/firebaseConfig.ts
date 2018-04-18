9dee661dec0df8c354ae5114ada588584de157a3
Christopher Woods <cjwoods89@gmail.com>
2018-04-18 16:07:59 -0400

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyA6PI8oXYmt-e1lC_RF2X0Z_kvKrA3VBgs",
+    authDomain: "cah-devs.firebaseapp.com",
+    databaseURL: "https://cah-devs.firebaseio.com/",
+    storageBucket: "gs://cah-devs.appspot.com/",
+    messagingSenderId: "433452554170"
+};
+
